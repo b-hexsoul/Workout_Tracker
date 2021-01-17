00bb8ea1224d622afd582b27951093db03a4459a
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-01-17 06:06:13 -0800

    'Final cleanup and workout in range'

--- controller/workouts.js
+++ controller/workouts.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const db = require("../models/index");
 
+// @Desc Get all workouts
+// @Route GET /api/workouts
+// @access Public
 exports.getLastWorkout = async (req, res, next) => {
   console.log("getting workouts");
   try {
     // All documents enter aggregation pipeline and totalDuration is set as a field
     const workouts = await db.Workout.aggregate([
       {
-        $set: {
+        $addFields: {
           totalDuration: { $sum: "$exercises.duration" },
         },
       },
@@ -26,6 +29,9 @@ exports.getLastWorkout = async (req, res, next) => {
   }
 };
 
+// @Desc Create a workout
+// @Route POST /api/workouts
+// @access Public
 exports.createWorkout = async (req, res, next) => {
   console.log("creating a workout");
   try {
@@ -45,6 +51,9 @@ exports.createWorkout = async (req, res, next) => {
   }
 };
 
+// @Desc Add an exercise to a specific workout
+// @Route GET /api/workouts/:id
+// @access Public
 exports.addExercise = async (req, res, next) => {
   console.log("adding an exercise");
   let id = req.params.id;
@@ -67,6 +76,37 @@ exports.addExercise = async (req, res, next) => {
   }
 };
 
+// @Desc Get the past 7 workouts
+// @Route GET /api/workouts/range
+// @access Public
 exports.getWorkoutsInRange = async (req, res, next) => {
   console.log("getting workouts in range");
+  try {
+    const workouts = await db.Workout.aggregate([
+      {
+        $sort: {
+          day: -1,
+        },
+      },
+      {
+        $limit: 7,
+      },
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      success: true,
+      data: workouts,
+    });
+  } catch (error) {
+    // Send error response code and data
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
