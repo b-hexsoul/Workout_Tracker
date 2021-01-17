7922581b1614b0c0c4f5406305a7b541914d23ed
Brandon Hexsel <66162988+b-hexsoul@users.noreply.github.com>
2021-01-17 05:25:17 -0800

    'addExercise'

--- controller/workouts.js
+++ controller/workouts.js
@@ -22,12 +22,13 @@ exports.getLastWorkout = async (req, res, next) => {
 exports.createWorkout = async (req, res, next) => {
   console.log("creating a workout");
   try {
-    const workout = db.Workout.create(req.body);
+    const workout = await db.Workout.create(req.body);
 
     res.status(200).json({
       success: true,
       data: workout,
     });
+    console.log(workout);
   } catch (error) {
     // Send error response code and data
     res.status(400).json({
@@ -39,6 +40,24 @@ exports.createWorkout = async (req, res, next) => {
 
 exports.addExercise = async (req, res, next) => {
   console.log("adding an exercise");
+  let id = req.params.id;
+
+  try {
+    const exercise = await db.Workout.findByIdAndUpdate({ _id: id }, { $push: { exercises: req.body } }, { new: true });
+
+    res.status(200).json({
+      success: true,
+      data: exercise,
+    });
+
+    console.log(exercise);
+  } catch (error) {
+    // Send error response code and data
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 exports.getWorkoutsInRange = async (req, res, next) => {
--- public/exercise.js
+++ public/exercise.js
@@ -12,7 +12,7 @@ const distanceInput = document.querySelector("#distance");
 const completeButton = document.querySelector("button.complete");
 const addButton = document.querySelector("button.add-another");
 const toast = document.querySelector("#toast");
-const newWorkout = document.querySelector(".new-workout")
+const newWorkout = document.querySelector(".new-workout");
 
 let workoutType = null;
 let shouldNavigateAway = false;
@@ -21,13 +21,12 @@ async function initExercise() {
   let workout;
 
   if (location.search.split("=")[1] === undefined) {
-    workout = await API.createWorkout()
-    console.log(workout)
+    workout = await API.createWorkout();
+    console.log(workout);
   }
   if (workout) {
-    location.search = "?id=" + workout._id;
+    location.search = "?id=" + workout.data._id;
   }
-
 }
 
 initExercise();
@@ -151,6 +150,4 @@ if (addButton) {
 }
 toast.addEventListener("animationend", handleToastAnimationEnd);
 
-document
-  .querySelectorAll("input")
-  .forEach(element => element.addEventListener("input", validateInputs));
+document.querySelectorAll("input").forEach((element) => element.addEventListener("input", validateInputs));
